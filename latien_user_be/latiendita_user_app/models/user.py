2009eb30fe9cd17c1264c39a083c1dcd9384b032
MiguelC <miguelcardenaslugo@gmail.com>
2021-11-21 16:11:45 -0500

    Se modifica microserivsio usuarios

--- latien_user_be/latiendita_user_app/models/user.py
+++ latien_user_be/latiendita_user_app/models/user.py
@@ -1,21 +1,14 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-#from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    username = models.CharField('Username', max_length = 15, unique=True)
-    password = models.CharField('Password', max_length = 256)
-    name = models.CharField('Name', max_length = 30)
-    email = models.EmailField('Email', max_length = 100)
-    is_active = models.BooleanField(default=True)
-    
-"""
 class UserManager(BaseUserManager):
     def create_user(self, username, password=None):
-        
-        #Creates and saves a user with the given username and password.
-
+        """
+        Creates and saves a user with the given username and password.
+        """
         if not username:
             raise ValueError('Users must have an username')
         user = self.model(username=username)
@@ -24,8 +17,9 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, password):
-        #Creates and saves a superuser with the given username and password.
-      
+        """
+        Creates and saves a superuser with the given username and password.
+        """
         user = self.create_user(
             username=username,
             password=password,
@@ -33,19 +27,17 @@ class UserManager(BaseUserManager):
         user.is_admin = True
         user.save(using=self._db)
         return user
-
-class User(AbstractBaseUser, PermissionsMixin):
+    
+class User(models.Model):
     id = models.BigAutoField(primary_key=True)
     username = models.CharField('Username', max_length = 15, unique=True)
     password = models.CharField('Password', max_length = 256)
     name = models.CharField('Name', max_length = 30)
     email = models.EmailField('Email', max_length = 100)
-
+    is_active = models.BooleanField(default=True)
     def save(self, **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN' 
         self.password = make_password(self.password, some_salt)
         super().save(**kwargs)
-
     objects = UserManager()
     USERNAME_FIELD = 'username'
-"""
