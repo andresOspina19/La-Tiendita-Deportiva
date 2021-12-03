37ef3789dd92a8dbd63b9777b197b02334cd5a68
Andres Felipe Ospina <anfeosme@gmail.com>
2021-12-03 05:19:57 -0500

    Metodo para la paginacion

--- Backend/Inventory_ms/src/main/java/com/latienditadeportiva/Inventory_ms/repositories/ProductRepository.java
+++ Backend/Inventory_ms/src/main/java/com/latienditadeportiva/Inventory_ms/repositories/ProductRepository.java
@@ -1,6 +1,13 @@
 package com.latienditadeportiva.Inventory_ms.repositories;
 
 import com.latienditadeportiva.Inventory_ms.models.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository <Product, String> { }
+import org.springframework.data.domain.Pageable;
+
+public interface ProductRepository extends MongoRepository <Product, String> {
+    Page<Product> findByProductNameOrDescriptionIgnoreCaseContaining(String productName, String description, Pageable pageable);
+    Page<Product> findByProductNameIgnoreCaseContaining(String productName, Pageable pageable);
+
+}
