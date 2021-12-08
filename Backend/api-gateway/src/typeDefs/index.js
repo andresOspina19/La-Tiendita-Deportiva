c9d50d24ba50ac4988340092d65325c867758e75
Andres Felipe Ospina <anfeosme@gmail.com>
2021-12-08 03:10:53 -0500

    Pequeño adelanto del carrito de compras y pedidos en el API gateway

--- Backend/api-gateway/src/typeDefs/index.js
+++ Backend/api-gateway/src/typeDefs/index.js
@@ -3,9 +3,11 @@ const productTypeDefs = require('./product_type_defs');
 const inventoryTypeDefs = require('./inventory_type_defs');
 const authTypeDefs = require('./auth_type_defs');
 const payTypeDefs = require('./pay_type_defs');
+const cartTypeDefs = require('./cart_type_defs');
+const orderTypeDefs = require('./order_type_defs');
 
 //Se unen
-const schemasArrays = [authTypeDefs, productTypeDefs, inventoryTypeDefs, payTypeDefs];
+const schemasArrays = [authTypeDefs, productTypeDefs, inventoryTypeDefs, payTypeDefs, cartTypeDefs, orderTypeDefs];
 
 //Se exportan
 module.exports = schemasArrays;
---
+++ Backend/api-gateway/src/typeDefs/order_type_def.js
@@ -0,0 +1,6 @@
+const { gql } = require('apollo-server');
+
+const orderTypeDefs = gql `
+`;
+
+module.exports = orderTypeDefs;
