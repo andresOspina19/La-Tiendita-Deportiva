d5f4f8bcce4e5832b4669b964632f1114239747d
MiguelC <miguelcardenaslugo@gmail.com>
2021-12-11 19:42:16 -0500

    Cambios front

--- Frontend/la_tiendita_deportiva_fe/src/components/SignUp.vue
+++ Frontend/la_tiendita_deportiva_fe/src/components/SignUp.vue
@@ -92,28 +92,30 @@ export default {
 
 //Pendiente por modificar
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap');
     .signUp_user{
         margin: 0;
         padding: 0%;
         height: 100%;
         width: 100%;
-    
         display: flex;
         justify-content: center;
         align-items: center;
+        font-family:  comfortaa;
+        font-size: 100%;
     }
 
     .container_signUp_user {
         border: 3px solid  #283747;
         border-radius: 10px;
-        width: 25%;
-        height: 60%;
-        
+        width: auto;
+        height: auto;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        font-family:  comfortaa;
+        font-size: 100%;
     }
 
     .signUp_user h2{
@@ -127,32 +129,33 @@ export default {
     }
 
     .signUp_user input{
-        height: 40px;
+      height: auto;
         width: 100%;
-
+        font-family: comfortaa;
+        font-size: 100%;
         box-sizing: border-box;
         padding: 10px 20px;
         margin: 5px 0;
-
         border: 1px solid #283747;
+        border-radius: 20px;
     }
 
     .signUp_user button{
         width: 100%;
         height: 40px;
-
-        color: #E5E7E9;
+         color: #E5E7E9;
         background: #283747;
         border: 1px solid #E5E7E9;
-
-        border-radius: 5px;
+        border-radius: 18px;
         padding: 10px 25px;
         margin: 5px 0 25px 0;
+        font-family:  comfortaa;
+        font-size: 100%;
     }
 
     .signUp_user button:hover{
-        color: #E5E7E9;
-        background: crimson;
+         color: #E5E7E9;
+        background: rgb(137, 176, 228);
         border: 1px solid #283747;
     }
 
